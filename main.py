568fb9570eb666b96133cb4e783e8d41eae4142f
Kevin D <kevinjosediaz15@gmail.com>
2021-10-20 22:33:33 +0000

    1.)Deleted file scanner, because it wasn't fully implemented (Will do later).
    2.)Put link scanner implementation within a try except

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ from keep_alive import keep_alive
 client = discord.Client()
 
 urlApiLink = 'https://www.virustotal.com/vtapi/v2/url/report'
-fileApiLink = 'https://www.virustotal.com/vtapi/v2/file/report'
 
 @client.event
 async def on_ready():
@@ -22,33 +21,11 @@ async def on_message(message):
   
   ############################URLs###################################
   #Scans Link through VirusTotal API; Returns any detected positive results and prints to console
-  foundLink = re.search("(?P<url>https?://[^\s]+)", message.content).group("url")
+  try:
+    foundLink = re.search("(?P<url>https?://[^\s]+)", message.content).group("url")
     
-  params = {'apikey': os.environ['Key'], 'resource': foundLink}
-  response = requests.get(urlApiLink, params=params)
-  response_json = json.loads(response.content)
-  positiveEngineList = []
-
-  for i in response_json["scans"]:
-      if(response_json["scans"][i]["detected"] == True):
-        positiveEngineList.append(i)
-
-  if(response_json["positives"] > 0):
-    await message.channel.send("CAUTION: " + str(response_json["positives"]) + " out of " + str(response_json["total"]) + " vendors flagged this URL as malicious: " + str(positiveEngineList))
-    
-  if(response_json["positives"] <= 0):
-    await message.channel.send("Safe URL")
-    
-  return
-
-  
-
-  ############################FILES##################################
-  try: #Scans file through VirusTotal API; Returns any detected positive results and prints to console
-    foundfile = re.search("(?P<url>https?://[^\s]+)", message.content).group("url")
-    
-    params = {'apikey': os.environ['Key'], 'resource': foundfile}
-    response = requests.get(fileApiLink, params=params)
+    params = {'apikey': os.environ['Key'], 'resource': foundLink}
+    response = requests.get(urlApiLink, params=params)
     response_json = json.loads(response.content)
     positiveEngineList = []
 
@@ -58,13 +35,13 @@ async def on_message(message):
 
     if(response_json["positives"] > 0):
       await message.channel.send("CAUTION: " + str(response_json["positives"]) + " out of " + str(response_json["total"]) + " vendors flagged this URL as malicious: " + str(positiveEngineList))
-    
+      
     if(response_json["positives"] <= 0):
-      await message.channel.send("SAFE: " + str(response_json["positives"]) + " out of " + str(response_json["total"]) + " vendors flagged this URL as malicious")
-
+      await message.channel.send("Safe URL")
+    
     return
+
   except:
-    print("couldn't process: " + str(foundfile))
     pass
 
 keep_alive()
